0c1df87a53624cd532235ad433d6a813a839214a
zhuoyikang <zhuoyikang@gmail.com>
2016-04-23 10:21:22 +0800

    slash_sup

---
+++ src/slash_sup_app.erl
@@ -0,0 +1,16 @@
+-module(slash_sup_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    slash_sup_sup:start_link().
+
+stop(_State) ->
+    ok.
