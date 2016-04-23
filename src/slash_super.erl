1dd2722b1015ba9068049bf36cc12c8be96f7ee7
zhuoyikang <zhuoyikang@gmail.com>
2016-04-23 14:07:13 +0800

    原型

--- src/slash_super.erl
+++ src/slash_super.erl
@@ -9,3 +9,14 @@ get(Node) ->
     PidList = [Pid || #super{pid=Pid} <- List],
     lists:nth(random:uniform(length(PidList)), PidList).
 
+%% 同步启动，等待结果.
+start_sync(Node, Spec) ->
+    Pid = slash_super:get(Node),
+    gen_server:call(Pid, {start_child, Spec}).
+
+%% 异步启动，传入回调函数.
+start_async(Node, Spec, Fun) ->
+    Pid = slash_super:get(Node),
+    gen_server:cast(Pid, {start_child, Spec, Fun}).
+
+
